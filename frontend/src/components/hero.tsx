bac0787db329611de624c77ddb573e635bfd2e6d
tarunmeena6846 <tarunmeena6846@gmail.com>
2024-08-19 14:00:17 +0530

    hero UI changes

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -7,44 +7,6 @@ const getRandomPosition = (maxWidth, maxHeight) => {
   return { left, top };
 };
 const Hero = () => {
-  // const containerRef = useRef(null);
-
-  // useEffect(() => {
-  //   const container = containerRef.current;
-  //   const containerWidth = container.offsetWidth;
-  //   const containerHeight = container.offsetHeight;
-
-  //   const images = container.querySelectorAll("img");
-  //   images.forEach((img) => {
-  //     const { left, top } = getRandomPosition(
-  //       containerWidth - img.width,
-  //       containerHeight - img.height
-  //     );
-  //     img.style.position = "absolute";
-  //     img.style.left = `${left}px`;
-  //     img.style.top = `${top}px`;
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrollY = window.scrollY;
-  //     // Adjust the threshold based on your preference
-  //     const scrollThreshold = 100;
-
-  //     setIsScrolled(scrollY > scrollThreshold);
-  //   };
-
-  //   // Add this check to ensure it runs only on the client side
-  //   if (typeof window !== "undefined") {
-  //     window.addEventListener("scroll", handleScroll);
-
-  //     return () => {
-  //       window.removeEventListener("scroll", handleScroll);
-  //     };
-  //   }
-  // }, []);
-
   return (
     <div className="flex flex-row justify-center py-20 gap-10">
       <div className="w-1/2 mt-30">
@@ -70,19 +32,50 @@ const Hero = () => {
         </motion.button>
       </div>
       <div className="w-1/2 h-screen relative">
-        <div className="absolute top-[-110px] left-[10px] w-40 h-30">
+        <div className="absolute top-[-50px] left-[10px] w-40 h-30">
           <div className="absolute inset-0 bg-[#e9a0b0] rounded-2xl transform translate-x-2 translate-y-2"></div>
           <div className="absolute inset-0 bg-[#e9a0b0] rounded-2xl transform translate-x-4 translate-y-4"></div>
-          <img src="./landing.png" className="relative rounded-2xl shadow-lg" />
-        </div>
 
+          <img
+            src="./landing.png"
+            className="relative rounded-2xl shadow-lg z-[30]"
+          />
+          <div className="absolute bottom-10 left-0 z-[100]">
+            <motion.img
+              src="./hero1.svg"
+              className="w-10"
+              animate={{
+                y: [0, 20, 0], // Moves 20px to the right and then back
+              }}
+              transition={{
+                duration: 3, // Duration of one complete cycle (to-and-fro)
+                repeat: Infinity, // Infinite repeat
+                ease: "easeInOut", // Easing function for smooth motion
+              }}
+            />
+          </div>
+        </div>
         <div className="absolute bottom-[130px] left-[50px] w-40 ">
           <div className="absolute inset-0 bg-[#75bcda] rounded-2xl transform translate-x-2 translate-y-2"></div>
           <div className="absolute inset-0 bg-[#75bcda] rounded-2xl transform translate-x-4 translate-y-4"></div>
           <img
             src="./landing1.png"
-            className="relative rounded-2xl shadow-lg"
+            className="relative rounded-2xl shadow-lg z-[30]"
           />
+          <div className="absolute bottom-[-150px] right-[-150px] z-[100]">
+            <motion.img
+              src="./hero5.svg"
+              // className="w-10"
+              // animate={{
+              //   y: [0, 20, 0], // Moves 20px to the right and then back
+              // }}
+              // transition={{
+              //   duration: 3, // Duration of one complete cycle (to-and-fro)
+              //   repeat: Infinity, // Infinite repeat
+              //   ease: "easeInOut", // Easing function for smooth motion
+              // }}
+            />
+          </div>
         </div>
 
         <div className="absolute top-[30px] right-[50px] w-40">
@@ -92,10 +85,51 @@ const Hero = () => {
             src="./landing2.png"
             className="relative rounded-2xl shadow-lg"
           />
+          <div className="absolute top-[-10px] left-[-30px] z-[100]">
+            <motion.img
+              src="./hero2.svg"
+              className="w-20"
+              animate={{
+                rotate: 360, // Rotate the image to complete the circle
+              }}
+              transition={{
+                duration: 8, // Duration of one full circle
+                ease: "linear", // Smooth motion
+                repeat: Infinity, // Infinite loop
+              }}
+            />
+          </div>
+          <div className="absolute bottom-[-30px] right-[-20px] z-[100]">
+            <motion.img
+              src="./hero3.svg"
+              className="w-[50px]"
+              animate={{
+                y: [0, 20, 0], // Moves 20px to the right and then back
+              }}
+              transition={{
+                duration: 3, // Duration of one complete cycle (to-and-fro)
+                repeat: Infinity, // Infinite repeat
+                ease: "easeInOut", // Easing function for smooth motion
+              }}
+            />
+          </div>
         </div>
-
         <div className="absolute bottom-[70px] right-[50px] w-40">
           <img src="./Graph.png" className="relative rounded-2xl shadow-lg" />
+          <div className="absolute top-[-30px] left-[50px] z-[100]">
+            <motion.img
+              src="./hero4.svg"
+              className="w-[50px]"
+              animate={{
+                y: [0, 20, 0], // Moves 20px to the right and then back
+              }}
+              transition={{
+                duration: 3, // Duration of one complete cycle (to-and-fro)
+                repeat: Infinity, // Infinite repeat
+                ease: "easeInOut", // Easing function for smooth motion
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
